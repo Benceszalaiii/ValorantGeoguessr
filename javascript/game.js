430d70f6c9491f69df5d24a315003e82136d27ed
simonadamgyula <simon.adam.gyula@students.jedlik.eu>
2023-03-30 15:15:11 +0200

    save username into cookies

--- javascript/game.js
+++ javascript/game.js
@@ -2,6 +2,7 @@ var x = 0;
 var y = 0;
 var loc;
 var map;
+var user;
 
 const MAX_POINT = 5000;
 const MAX_DISTANCE = 200 - 8;
@@ -13,144 +14,150 @@ var points = 0;
 var current_points = 0;
 
 function guess() {
-  if ($("#guess-btn").attr("disabled")) return;
+    if ($("#guess-btn").attr("disabled")) return;
 
-  rounds_played++;
-  console.log(loc.distance([x, y]));
-  point();
-  show_score();
+    rounds_played++;
+    console.log(loc.distance([x, y]));
+    point();
+    show_score();
 }
 
 function point() {
-  distance = loc.distance([x, y]);
+    distance = loc.distance([x, y]);
 
-  if (distance < 8) {
-    current_points = MAX_POINT;
-  } else {
-    dist = Math.floor((MAX_POINT / MAX_DISTANCE) * distance);
-    dist = dist >= MAX_POINT ? MAX_POINT : dist;
+    if (distance < 8) {
+        current_points = MAX_POINT;
+    } else {
+        dist = Math.floor((MAX_POINT / MAX_DISTANCE) * distance);
+        dist = dist >= MAX_POINT ? MAX_POINT : dist;
 
-    current_points = 5000 - dist;
-  }
+        current_points = 5000 - dist;
+    }
 
-  points += current_points;
+    points += current_points;
 
-  console.log(current_points, points);
+    console.log(current_points, points);
 }
 
 function show_score() {
-  $("#max").text(MAX_POINT * rounds_played);
-  $("#earned").text(points);
+    $("#max").text(MAX_POINT * rounds_played);
+    $("#earned").text(points);
 
-  $("#c-max").text(MAX_POINT);
-  $("#c-earned").text(current_points);
+    $("#c-max").text(MAX_POINT);
+    $("#c-earned").text(current_points);
 
-  $("#game").css("display", "none");
-  $("#score").css("display", "flex");
+    $("#game").css("display", "none");
+    $("#score").css("display", "flex");
 
-  $("#image").attr("src", "");
+    $("#image").attr("src", "");
 
-  $("#nav>button:first-child").on("click", () => {
-    if ($("#score").css("display") != "none") {
-      next_location();
-    }
-  });
+    $("#nav>button:first-child").on("click", () => {
+        if ($("#score").css("display") != "none") {
+            next_location();
+        }
+    });
 
-  $("#nav>button:last-child").on("click", () => {
-    location = "index.html";
-  });
+    $("#nav>button:last-child").on("click", () => {
+        location = "index.html";
+    });
 }
 
 function next_location() {
-  $("#score").css("display", "none");
-  $("#game").css("display", "block");
+    $("#score").css("display", "none");
+    $("#game").css("display", "block");
 
-  if (rounds_played == MAX_ROUND) {
-    location = "index.html";
-  }
+    if (rounds_played == MAX_ROUND) {
+        location = "index.html";
+    }
 
-  loc = locs_selected[rounds_played];
+    loc = locs_selected[rounds_played];
 
-  $("#image").attr("src", loc.map);
-  $("body").css("background-image", `url(${loc.map})`);
+    $("#image").attr("src", loc.map);
+    $("body").css("background-image", `url(${loc.map})`);
 
-  $("#marker").css("right", "-20px");
-  $("#guess-btn").attr("disabled", true);
-  $("#guess-btn").text("Place a ping on the map");
+    $("#marker").css("right", "-20px");
+    $("#guess-btn").attr("disabled", true);
+    $("#guess-btn").text("Place a ping on the map");
 }
 
 $("#map").click(function (e) {
-  var offset = $(this).offset();
-  x = e.pageX - offset.left;
-  y = e.pageY - offset.top;
+    var offset = $(this).offset();
+    x = e.pageX - offset.left;
+    y = e.pageY - offset.top;
 
-  $("#marker").css(
-    "right",
-    `${
-      $(window).width() - (offset.left + $(this).outerWidth()) + (500 - x) - 2.5
-    }px`
-  );
-  $("#marker").css(
-    "bottom",
-    `${
-      $(window).height() -
-      (offset.top + $(this).outerHeight()) +
-      (500 - y) -
-      2.5
-    }px`
-  );
+    $("#marker").css(
+        "right",
+        `${$(window).width() - (offset.left + $(this).outerWidth()) + (500 - x) - 2.5
+        }px`
+    );
+    $("#marker").css(
+        "bottom",
+        `${$(window).height() -
+        (offset.top + $(this).outerHeight()) +
+        (500 - y) -
+        2.5
+        }px`
+    );
 
-  $("#coords").text(`x: ${x}, y: ${y}`);
+    $("#coords").text(`x: ${x}, y: ${y}`);
 
-  $("#guess-btn").attr("disabled", false);
-  $("#guess-btn").text("GUESS");
+    $("#guess-btn").attr("disabled", false);
+    $("#guess-btn").text("GUESS");
 });
 
 $("#guess-btn").on("click", () => {
-  guess();
+    guess();
 });
 document.addEventListener("keydown", (e) => {
-  if (e.code == "Space") {
-    if ($("#score").css("display") != "none") {
-      next_location();
-      return;
+    if (e.code == "Space") {
+        if ($("#score").css("display") != "none") {
+            next_location();
+            return;
+        }
+        guess();
     }
-    guess();
-  }
 });
 
 function main() {
-  map = sessionStorage.getItem("map");
+    map = sessionStorage.getItem("map");
+    user = sessionStorage.getItem("username")
 
-  if (map == "Random") {
-    alert("This feature is still WIP, try playing another one!");
-    location = "index.html";
-  return}
-  $("#map").attr("src", `/sources/minimaps/${map.toLocaleLowerCase()}.png`);
+    if (map == null) {
+        alert("WRONG TURN?\nYou haven't selected a map yet!");
+        location = "index.html";
+        return;
+    }
 
-  // section: TO BE CHANGED WHEN NEW LOCATIONS ARE UPLOADED
+    if (map == "Random") {
+        alert("This feature is still WIP, try playing another one!");
+        location = "index.html";
+        return
+    }
+    $("#map").attr("src", `/sources/minimaps/${map.toLocaleLowerCase()}.png`);
 
-  try {
-    locations[map.toLowerCase()]["easy"].length;
-  } catch {
-    alert("This map is still WIP, try playing another one!");
-    location = "index.html";
-  }
-  if (locations[map.toLowerCase()]["easy"].length < 5) {
-    alert("This map is still WIP, try playing another one!");
-    location = "index.html";
-  }
-  do {
-    var current =
-      locations[map.toLowerCase()]["easy"][
-        Math.floor(Math.random() * locations[map.toLowerCase()]["easy"].length)
-      ];
-    if (!locs_selected.includes(current)) locs_selected.push(current);
-  } while (locs_selected.length < 5);
+    // section: TO BE CHANGED WHEN NEW LOCATIONS ARE UPLOADED
 
-  // end_section
+    try {
+        locations[map.toLowerCase()]["easy"].length;
+    } catch {
+        alert("This map is still WIP, try playing another one!");
+        location = "index.html";
+    }
+    if (locations[map.toLowerCase()]["easy"].length < 5) {
+        alert("This map is still WIP, try playing another one!");
+        location = "index.html";
+    }
+    do {
+        var current =
+            locations[map.toLowerCase()]["easy"][
+            Math.floor(Math.random() * locations[map.toLowerCase()]["easy"].length)
+            ];
+        if (!locs_selected.includes(current)) locs_selected.push(current);
+    } while (locs_selected.length < 5);
 
-  next_location();
+    // end_section
+
+    next_location();
 }
 
 main();
--- javascript/index.js
+++ javascript/index.js
@@ -7,3 +7,25 @@ $("button").on("click", function() {
     sessionStorage.setItem("map", map);
     window.location.href = "game.html";
 });
+
+function main() {
+    var user = getCookie("username");
+
+    if (user == "") {
+        $("#user-input").css("display", "flex");
+        $("input#submit").on("click", function() {
+            var input = $("input#name").val();
+            if (input == "" || input == null)
+                return;
+
+            user = input;
+            setCookie("username", user, 10);
+
+            $("#user-input").css("display", "none");
+        });
+    }
+
+    sessionStorage.setItem("user", user);
+}
+
+main();
