e25b296935b9372d9ec071a7990fd09675d61af0
simonadamgyula <simon.adam.gyula@students.jedlik.eu>
2023-04-01 23:04:38 +0200

    menu done

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,6 @@
 
 $("#maps>img").on("click", function() {
     var map = $(this).attr("id");
-    console.log(map)
-
 
     sessionStorage.setItem("map", map);
     window.location.href = "options.html";
@@ -11,11 +9,31 @@ $("#maps>img").on("click", function() {
 
 $("#maps>img").hover(function(){
     var map = $(this).attr("id");
-    $("#map-name").text(map)
+    $("#map-name").text(map);
 },
 function(){
-    $("#map-name").text("​")
-})
+    $("#map-name").text("​");
+});
+
+$("#header > :first-child").on("click", function() {
+    $("#menu").removeClass("closed");
+    $("#close").css("display", "inline-block");
+    $("#cover").css("display", "block");
+});
+
+function close_menu() {
+    $("#menu").addClass("closed");
+    $("#close").css("display", "none");
+    $("#cover").css("display", "none");
+}
+
+$("#change-name").on("click", () => {
+    deleteCookie("username");
+    location = "index.html";
+});
+
+$("#close").on("click", () => close_menu());
+$("#cover").on("click", () => close_menu());
 
 function main() {
     var user = getCookie("username");
@@ -23,23 +41,30 @@ function main() {
     if (user == "") {
         $("#grid").css("visibility","hidden")
         $("#user-input").css("display", "flex");
-        $("input#submit").on("click", function() {
-            var input = $("input#name").val();
+        $("input#submit-name").on("click", function() {
+            var input = $("input#name-input").val();
             if (input == "" || input == null)
                 return;
         
             user = input;
             setCookie("username", user, 10);
+
+            sessionStorage.setItem("username", user);
+            $("#name").text(user);
+
             $("#grid").css("visibility","visible")
             $("#user-input").css("display", "none");
         });
         $("#guest").on("click", function(){
-            $("#grid").css("visibility","visible")
+            $("#grid").css("visibility","visible");
             $("#user-input").css("display", "none");
+
+            $("#name").text("*Guest*");
         })
     }
 
     sessionStorage.setItem("username", user);
+    $("#name").text(user);
 }
 
 main();
