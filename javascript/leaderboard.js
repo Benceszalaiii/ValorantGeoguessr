15428fc31eb38ea0c23d18f71f6c33503ffb1ed9
szephalmiaron <szephalmiaron@students.jedlik.eu>
2023-04-03 13:09:39 +0200

    leaderboard filter

--- javascript/leaderboard.js
+++ javascript/leaderboard.js
@@ -1,5 +1,13 @@
+var map = $("#Map").val()
+var difficulty = $("#Difficulty").val()
+var sort;
+
 get(function (response, args) {
     response.sort(function (a, b) {return (a.point - b.point) * -1});
+    map = $("#Map").val()
+    difficulty = $("#Difficulty").val()
+
+    console.log(map,difficulty)
 
     table = "";
 
@@ -10,3 +18,29 @@ get(function (response, args) {
 
     $("#leaderboard>tbody").html(table)
 }, {})
+
+$("#Map").change(function(){
+    map = $(this).val()
+    select()
+})
+
+$("#Difficulty").change(function(){
+    difficulty = $(this).val()
+    select()
+})
+
+function select(){
+    get(function (response, args){
+        response.sort(function (a, b) {return (a.point - b.point) * -1});
+        sort = response.filter(val => (map == "All" ? "All" : val.map == map) && (difficulty == "All" ? "All" : val.difficulty == difficulty) ) 
+
+        table = ""
+        for (i = 0; i < sort.length; i++) {
+            user = sort[i]
+            table = table + `<tr><td>#${i + 1}</td><td>${user.name}</td><td>${user.point}</td><td>${user.time} sec</td><td>${user.map}</td><td>${user.difficulty}</td></tr>`
+        }
+
+        $("#leaderboard>tbody").html(table)
+    },{})
+
+}
