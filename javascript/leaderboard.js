7e8a094e87b038cc7d50184f67dc699fabe9b9c8
szephalmiaron <szephalmiaron@students.jedlik.eu>
2023-04-03 12:35:32 +0200

    leadboard style

--- javascript/leaderboard.js
+++ javascript/leaderboard.js
@@ -3,7 +3,7 @@ get(function (response, args) {
     tops = response.slice(0, 10);
 
     table = "";
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < tops.lenght; i++) {
         user = tops[i]
         table = table + `<tr><td>#${i + 1}</td><td>${user.name}</td><td>${user.point}</td><td>${user.map}</td></tr>`
     }
